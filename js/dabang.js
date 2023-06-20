ee1e0db970a566f3d08198bb8078751f534acf03
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-06-20 18:14:48 +0900

    Add files via upload

--- js/dabang.js
+++ js/dabang.js
@@ -7,13 +7,33 @@ $(function () {
     });
 
     $('.main_Slide').slick({
-        vertical: false,
-        autoplay: false,
-        autoplaySpeed: 5000,
+        // vertical: true,
+        // verticalSwiping: true,
         arrows: false,
-        dots: true,
-        slidesToShow: 1,
+        dots: false,
+        // speed: 1000,
+        fade: true,
+    });
+
+    $('.main_Visual .arrows .up').on('click', function () {
+        $('.main_Slide').slick('slickPrev')
+    });
+    $('.main_Visual .arrows .down').on('click', function () {
+        $('.main_Slide').slick('slickNext')
     });
 
 
-})
+    $('.mini_Slide01').slick({
+        vertical: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 2,
+    });
+
+    // $('.mini_Slide02').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     // arrows: false,
+    // });
+});
